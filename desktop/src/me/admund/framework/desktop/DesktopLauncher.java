685bda94b6d1377d81af66824977dfbce973dfb5
Adrian Gardian <gardian.adrian@gmail.com>
2014-12-29 17:38:05 +0100

    HTML and desktop game_size

--- desktop/src/me/admund/framework/desktop/DesktopLauncher.java
+++ desktop/src/me/admund/framework/desktop/DesktopLauncher.java
@@ -3,10 +3,13 @@ package me.admund.framework.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import me.admund.framework.FrameworkTest;
+import me.admund.framework.GameConfig;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = GameConfig.GAME_WIDTH;
+		config.height = GameConfig.GAME_HEIGHT;
 		new LwjglApplication(new FrameworkTest(), config);
 	}
 }
