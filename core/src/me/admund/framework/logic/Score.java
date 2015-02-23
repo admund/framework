6544ac828c29f2b177613da1ae976f09cb69a38b
Adrian Gardian <gardian.adrian@gmail.com>
2015-02-23 23:19:00 +0100

    [Framework] add GamePreferences

--- core/src/me/admund/framework/logic/Score.java
+++ core/src/me/admund/framework/logic/Score.java
@@ -6,13 +6,14 @@ import com.badlogic.gdx.utils.ObjectMap;
  * Created by admund on 2015-01-11.
  */
 public class Score {
-    private static final Score inst = new Score();
-    private Score(){}
-    public static Score inst(){
-        return inst;
-    }
+    private GamePreferences pref = null;
 
     private ObjectMap<String, Float> scoreMap = new ObjectMap<String, Float>();
+
+    public void init(GamePreferences pref) {
+        this.pref = pref;
+        this.pref.init();
+    }
 
     public float getScore(String scoreName) {
         Float tmp = scoreMap.get(scoreName);
@@ -32,7 +33,22 @@ public class Score {
         scoreMap.put(scoreName, tmp.floatValue() + value);
     }
 
+    public void setScore(String scoreName, float value) {
+        Float tmp = scoreMap.get(scoreName);
+        if(tmp == null) {
+            tmp = new Float(0);
+            scoreMap.put(scoreName, tmp);
+        }
+        scoreMap.put(scoreName, value);
+    }
+
     public void clear() {
         scoreMap.clear();
+    }
+
+    public void save() {
+        if(pref != null) {
+            pref.save(this);
+        }
     }
 }
