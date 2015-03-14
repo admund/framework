aeed5639eb20829a0555965304f59e8b4e667ee9
Adrian Gardian <gardian.adrian@gmail.com>
2015-03-14 20:09:06 +0100

    [Framework] achievements provider Android

--- android/src/me/admund/framework/android/AndroidLauncher.java
+++ android/src/me/admund/framework/android/AndroidLauncher.java
@@ -1,15 +1,40 @@
 package me.admund.framework.android;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
+import me.admund.framework.FrameworkTest;
+
 public class AndroidLauncher extends AndroidApplication {
+
+	private AndroidAchivmentsProvider provider = null;
+
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new FrameworkTest(null), config);
+		provider = new AndroidAchivmentsProvider(this);
+		initialize(new FrameworkTest(provider), config);
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		provider.onStart(this);
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		provider.onStop();
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		provider.onActivityResult(requestCode, resultCode, data);
 	}
 }
